0d84412e0954ee40fa47225c606dccbcc1d80bc2
Siya_kalia <kaliasiya007@gmail.com>
2024-11-28 11:37:29 +0530

    oops

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //store 5 roll nos
+        int[] numbers = new int[5];
+        //store 5 names
+        String[] names = new String[5];
+        // data of 5 students:{roll no, name, marks}
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+        System.out.println("Hello world!");
+
+        Student[] students = new Student[5];
+        Student s1 ;
+        System.out.println(Arrays.toString(students));
+    }
+    // create a class
+    class Student{
+        int rno;
+        String name;
+        float marks;
+    }
+}
